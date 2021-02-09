6addbfa1c3aa5b8cc31cadf494834046b7e3f371
RamazanNurbaev <78719716+RamazanNurbaev@users.noreply.github.com>
2021-02-09 12:11:55 +0600

    Add files via upload

---
+++ praktika 2.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import math 
+a=input('Enter a:')
+a=int(a)
+b=input('Enter b:')
+b=int(b)
+c=input('Enter c:')
+c=int(c)
+p=(a+b+c)/2
+s=math.sqrt(p+(p-a)*(p-b)*(p-c))
+print ('s=',s)
+
+
+# In[ ]:
+
+
+import math 
+print('Enter a:')
+a=int(input())
+b=a**2
+z=b+4
+y=b/3-z/6+math.sqrt(z)/4+(math.sqrt(z*z*z))/4
+print ('y=',y)
+
+
+# In[ ]:
+
+
+print(92)
+n=input()
+x1=n%10
+x2=n//10
+m=x1*10+x2
+print(m)
+
+
+# In[ ]:
+
+
+print(92)
+n=input()
+x1=n%10
+x2=n//10
+m=x1*10+x2
+print(m)
+
+
+# In[ ]:
+
+
+print(25)
+n=input()
+x1=25%10=5
+x2=25//10=2
+m=x1*10+x2
+print(m)
+
+
+# In[ ]:
+
+
+print(25)
+n=input(25)
+x1=25%10=5
+x2=25//10=2
+m=x1*10+x2
+print(m)
+
+
+# In[ ]:
+
+
+import math 
+print('100:')
+a=float(input(2))
+x1=int(a)
+x2=a%1
+b=x1/100+x2*100
+print(b)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
