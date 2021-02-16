e7c03840d84c0a9837bc05c7bcc4c0c977faf917
RamazanNurbaev <78719716+RamazanNurbaev@users.noreply.github.com>
2021-02-16 09:36:46 +0600

    Add files via upload

---
+++ Praktika 3.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+if 1:
+    print("Hello 1")
+
+
+# In[4]:
+
+
+a=3
+if a==3:
+    print ("Здесь a=",a)
+
+
+# In[5]:
+
+
+if a>1:
+    print("hello 3")
+
+
+# In[7]:
+
+
+lst=[1,2,3]
+if lst:
+    print("Hello 4")
+
+
+# In[8]:
+
+
+a=3
+if a>2:
+    print("H")
+
+
+# In[13]:
+
+
+a=1
+if a>2:
+    print("H")
+else:
+    print("L")
+
+
+# In[ ]:
+
+
+a=int(input("92"))
+if a<0:
+    print("Neg")
+elif a==0:
+    print("Zero")
+else:
+    print("Pos")
+
+
+# In[ ]:
+
+
+
+
