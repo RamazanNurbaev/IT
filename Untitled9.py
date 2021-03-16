1ee4341682193279fe519703fdf9adbb11e79a9f
RamazanNurbaev <78719716+RamazanNurbaev@users.noreply.github.com>
2021-03-16 10:38:37 +0600

    Add files via upload

---
+++ Untitled9.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+number=int(input('д4з:'))
+n1=round(number//1000%10)
+n2=round(number//100%10)
+n3=round(number//10%10)
+n4=round(number//1%10)
+print('n1','n2','n3','n4',n1,n2,n3,n4)
+
+
+# In[ ]:
+
+
+
+
